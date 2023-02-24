c9a2d7562817ceac3b62e1e545b8c1d253c2daff
Emre Özgenç <emreozgenc@hotmail.com.tr>
2023-02-24 23:59:46 +0300

    Add project files.

---
+++ CustomMiddleware/MaximumFileSizeMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace MiddlewareExample.Middlewares
+{
+    public class MaximumFileSizeMiddleware : IMiddleware
+    {
+        private readonly ILogger<MaximumFileSizeMiddleware> _logger;
+        private readonly long _maximumBytes;
+
+        public MaximumFileSizeMiddleware(ILogger<MaximumFileSizeMiddleware> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            if (!long.TryParse(configuration["MaximumFileBytes"], out _maximumBytes))
+            {
+                throw new ArgumentException("\"MaximumFileBytes\" değeri ortam dosyasından okunamadı!");
+            }
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (context.Request.Form.Files.Any(file => file.Length > _maximumBytes))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync($"Yüklemeye çalıştığınız dosya veya dosyaların boyutu sınırın üzerindedir. ( En fazla : {_maximumBytes}B )");
+
+                string username = context.User.Identity.Name ?? "Anonymous";
+                _logger.LogWarning($"{username} adlı kullanıcı boyutu büyük dosya yüklemeye çalıştı.");
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}
---
+++ MiddlewareExample.Middlewares.Tests/MaximumFileSizeMiddlewareTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace MiddlewareExample.Middlewares.Tests
+{
+    public class MaximumFileSizeMiddlewareTests
+    {
+        [Fact]
+        public async Task OverMaximumSize_ShouldReturn_BadRequest()
+        {
+            var httpContext = GetHttpContext(4096);
+            var configuration = GetConfiguration("1024");
+            var logger = GetLogger();
+            var middleware = new MaximumFileSizeMiddleware(logger.Object, configuration);
+
+            await middleware.InvokeAsync(httpContext, RequestDelegate);
+
+            logger.Verify(x => x.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(),
+                    It.IsAny<It.IsAnyType>(),
+                    It.IsAny<Exception>(),
+                    (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Once);
+            Assert.Equal((int)HttpStatusCode.BadRequest, httpContext.Response.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task UnderMaximumSize_ShouldReturn_Ok()
+        {
+            var httpContext = GetHttpContext(128);
+            var configuration = GetConfiguration("1024");
+            var logger = GetLogger();
+            var middleware = new MaximumFileSizeMiddleware(logger.Object, configuration);
+
+            await middleware.InvokeAsync(httpContext, RequestDelegate);
+
+            logger.Verify(x => x.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(),
+                    It.IsAny<It.IsAnyType>(),
+                    It.IsAny<Exception>(),
+                    (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), Times.Never);
+            Assert.Equal((int)HttpStatusCode.OK, httpContext.Response.StatusCode);
+        }
+
+        private HttpContext GetHttpContext(int fileLength)
+        {
+            byte[] fileBytes = RandomNumberGenerator.GetBytes(fileLength);
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.ContentType = "multipart/form-data";
+            var fields = new Dictionary<string, StringValues>();
+            var formFiles = new FormFileCollection();
+            var formFile = new FormFile(
+                new MemoryStream(fileBytes),
+                0, fileBytes.Length, "source", "form-file.txt");
+            formFiles.Add(formFile);
+            httpContext.Request.Form = new FormCollection(fields, formFiles);
+
+            return httpContext;
+        }
+
+        private IConfiguration GetConfiguration(string fileLength)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var configurationCollection = new Dictionary<string, string>
+            {
+                { "MaximumFileBytes", fileLength }
+            };
+            configurationBuilder.AddInMemoryCollection(configurationCollection);
+            configurationBuilder.Properties.Add("MaximumFileBytes", fileLength);
+
+            return configurationBuilder.Build();
+        }
+        private Mock<ILogger<MaximumFileSizeMiddleware>> GetLogger()
+        {
+            return new Mock<ILogger<MaximumFileSizeMiddleware>>();
+        }
+
+        private async Task RequestDelegate(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            await context.Response.WriteAsync("Başarılı!");
+        }
+    }
+}
